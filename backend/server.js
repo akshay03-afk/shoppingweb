4db95bef3c3fe6c50b0eb1b27f5c091587de0795
akshay03-afk <akshaykumarchai1@gmail.com>
2021-04-07 18:42:30 +0545

    Modified directory for index

--- backend/server.js
+++ backend/server.js
@@ -14,7 +14,7 @@ dotenv.config()
 
 connectDB();
 
-const app = express()
+const app = express();
 
 if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'))
@@ -35,7 +35,7 @@ const __dirname = path.resolve()
 app.use('/uploads', express.static(path.join(__dirname, '/uploads')))
 
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '/frontend/build/index.html')))
+  app.use(express.static(path.join(__dirname, '/frontend')))
 
   app.get('*', (req, res) =>
     res.sendFile(path.resolve(__dirname, 'frontend','build' ,'index.html'))
