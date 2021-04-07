a81dc7c29aed6b3e87bd4ea54e2a024dcf79503e
akshay03-afk <akshaykumarchai1@gmail.com>
2021-04-07 18:33:56 +0545

    Modified directory

--- backend/server.js
+++ backend/server.js
@@ -35,10 +35,10 @@ const __dirname = path.resolve()
 app.use('/uploads', express.static(path.join(__dirname, '/uploads')))
 
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '/frontend/build')))
+  app.use(express.static(path.join(__dirname, '/frontend')))
 
   app.get('*', (req, res) =>
-    res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'))
+    res.sendFile(path.resolve(__dirname, 'frontend', 'index'))
   )
 } else {
   app.get('/', (req, res) => {
